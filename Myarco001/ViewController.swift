a028357e5e31357284f041a6cb872d06ee875b14
erle robinson <erlerob@gmail.com>
2017-07-18 09:48:42 -0400

    Initial Commit

---
+++ Myarco001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Myarco001
+//
+//  Created by erle robinson on 7/18/17.
+//  Copyright © 2017 erle robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
